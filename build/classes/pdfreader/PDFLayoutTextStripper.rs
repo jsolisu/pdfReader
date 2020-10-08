6a51c36219c31d84e9f5a2c11edf31c43aa5ba8a
jsolisu <jcsolisu2000@gmail.com>
2020-10-08 12:41:29 -0600

    Version inicial

---
+++ build/classes/pdfreader/PDFLayoutTextStripper.rs
@@ -0,0 +1,8 @@
+pdfreader.TextLine
+pdfreader.Character
+io.github.jonathanlink.CharacterFactory
+pdfreader.PDFLayoutTextStripper
+pdfreader.CharacterFactory
+io.github.jonathanlink.PDFLayoutTextStripper
+io.github.jonathanlink.Character
+io.github.jonathanlink.TextLine
